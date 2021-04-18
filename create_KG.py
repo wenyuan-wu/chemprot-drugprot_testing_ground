26a46292eedfdf178db186a3df025855c4946972
Wenyuan Wu <wenyuan.wu@uzh.ch>
2021-04-18 06:37:27 +0200

    create_KG.py created

--- create_KG.py
+++ create_KG.py
@@ -1,10 +1,26 @@
-import pandas as pd
+from data_inspection import get_df_from_data
 import networkx as nx
+import matplotlib.pyplot as plt
 
-col = []
-gs_df = pd.read_csv('data/chemprot_sample/chemprot_sample_gold_standard.tsv', sep='\t', header=None)
-print()
+abs_file_name, abs_df, ent_file_name, ent_df, gs_file_name, gs_df, rel_file_name, rel_df, pred_file_name, pred_df = \
+    get_df_from_data("sample")
 
-G = nx.Graph()
-G.add_node(1)
+kg_df = rel_df.copy()
+print(kg_df)
+kg_df["Arg1"] = kg_df["Arg1"].apply(lambda x: x[5:])
+kg_df["Arg2"] = kg_df["Arg2"].apply(lambda x: x[5:])
+# TODO: drop N entities
+index_names = kg_df[kg_df["Evaluation Type"] == "N"].index
+kg_df.drop(index_names, inplace=True)
+print(kg_df)
 
+G = nx.from_pandas_edgelist(kg_df, source="Arg1", target="Arg2", edge_attr="CPR Group", create_using=nx.MultiDiGraph)
+print(G.number_of_nodes())
+print(G.number_of_edges())
+# print(list(G.nodes))
+# print(list(G.edges))
+
+plt.figure(figsize=(12, 12))
+pos = nx.spring_layout(G, k=0.5)  # k regulates the distance between nodes
+nx.draw(G, with_labels=True, node_color='skyblue', node_size=1500, edge_cmap=plt.cm.Blues, pos=pos)
+plt.show()
--- data_inspection.py
+++ data_inspection.py
@@ -3,8 +3,38 @@ import pandas as pd
 
 
 def get_df_from_data(data_set="training"):
-    data_path = join("data", "chemprot_" + data_set)
+    """
+    Function to create pandas DataFrame from data files
+    :param data_set: string, type for dataset ["sample", "training", "development", "test"]
+    :return: file name and dataframe w.r.t. dataset
+    """
 
+    # to handle different naming convention in test set
+    if data_set == "test":
+        data_path = join("data", "chemprot_" + data_set + "_gs")
+        # abstracts
+        abs_file_name = join(data_path, "chemprot_" + data_set + "_abstracts_gs" + ".tsv")
+        abs_col_names = ["PMID", "Title", "Abstract"]
+        abs_df = pd.read_csv(abs_file_name, sep="\t", names=abs_col_names)
+
+        # entity mention annotations
+        ent_file_name = join(data_path, "chemprot_" + data_set + "_entities_gs" + ".tsv")
+        ent_col_names = ["PMID", "Entity #", "Type", "Start", "End", "Text"]
+        ent_df = pd.read_csv(ent_file_name, sep="\t", names=ent_col_names)
+
+        # Gold Standard data
+        gs_file_name = join(data_path, "chemprot_" + data_set + "_gold_standard" + ".tsv")
+        gs_col_names = ["PMID", "CPR Group", "Arg1", "Arg2"]
+        gs_df = pd.read_csv(gs_file_name, sep="\t", names=gs_col_names)
+
+        # ChemProt detailed relation annotations
+        rel_file_name = join(data_path, "chemprot_" + data_set + "_relations_gs" + ".tsv")
+        rel_col_names = ["PMID", "CPR Group", "Evaluation Type", "CPR", "Arg1", "Arg2"]
+        rel_df = pd.read_csv(rel_file_name, sep="\t", names=rel_col_names)
+
+        return abs_file_name, abs_df, ent_file_name, ent_df, gs_file_name, gs_df, rel_file_name, rel_df
+
+    data_path = join("data", "chemprot_" + data_set)
     # abstracts
     abs_file_name = join(data_path, "chemprot_" + data_set + "_abstracts" + ".tsv")
     abs_col_names = ["PMID", "Title", "Abstract"]
@@ -36,43 +66,12 @@ def get_df_from_data(data_set="training"):
     return abs_file_name, abs_df, ent_file_name, ent_df, gs_file_name, gs_df, rel_file_name, rel_df
 
 
-def get_df_from_data_test(data_set="test"):
-    data_path = join("data", "chemprot_" + data_set + "_gs")
-
-    # abstracts
-    abs_file_name = join(data_path, "chemprot_" + data_set + "_abstracts_gs" + ".tsv")
-    abs_col_names = ["PMID", "Title", "Abstract"]
-    abs_df = pd.read_csv(abs_file_name, sep="\t", names=abs_col_names)
-
-    # entity mention annotations
-    ent_file_name = join(data_path, "chemprot_" + data_set + "_entities_gs" + ".tsv")
-    ent_col_names = ["PMID", "Entity #", "Type", "Start", "End", "Text"]
-    ent_df = pd.read_csv(ent_file_name, sep="\t", names=ent_col_names)
-
-    # Gold Standard data
-    gs_file_name = join(data_path, "chemprot_" + data_set + "_gold_standard" + ".tsv")
-    gs_col_names = ["PMID", "CPR Group", "Arg1", "Arg2"]
-    gs_df = pd.read_csv(gs_file_name, sep="\t", names=gs_col_names)
-
-    # ChemProt detailed relation annotations
-    rel_file_name = join(data_path, "chemprot_" + data_set + "_relations_gs" + ".tsv")
-    rel_col_names = ["PMID", "CPR Group", "Evaluation Type", "CPR", "Arg1", "Arg2"]
-    rel_df = pd.read_csv(rel_file_name, sep="\t", names=rel_col_names)
-
-    return abs_file_name, abs_df, ent_file_name, ent_df, gs_file_name, gs_df, rel_file_name, rel_df
-
-
 def main():
-    data_set = "sample"
-    # data_set = "training"
-    # data_set = "development"
-    for i in get_df_from_data(data_set):
-        print(i)
-        print()
-
-    for i in get_df_from_data_test("test"):
-        print(i)
-        print()
+    data_set = ["sample", "training", "development", "test"]
+    for i in data_set:
+        for j in get_df_from_data(i):
+            print(j)
+            print()
 
 
 if __name__ == "__main__":
