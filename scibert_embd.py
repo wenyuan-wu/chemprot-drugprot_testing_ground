67040b4aadd36ae4c311bcebf057c7d17085afe8
Wenyuan Wu <wenyuan.wu@uzh.ch>
2021-05-14 04:19:29 +0200

    submodules added

---
+++ scibert_embd.py
@@ -0,0 +1,8 @@
+from transformers import *
+
+tokenizer = AutoTokenizer.from_pretrained('allenai/scibert_scivocab_uncased')
+model = AutoModel.from_pretrained('allenai/scibert_scivocab_uncased')
+
+doc = """
+
+"""
