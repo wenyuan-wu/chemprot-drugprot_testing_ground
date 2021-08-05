e81c38a0fe58983b41f20868adcb228acc61be07
wenyuan-wu <wenyuan.wu@uzh.ch>
2021-08-05 15:47:53 +0200

    bert_embd.py updated

---
+++ bert_embd.py
@@ -0,0 +1,92 @@
+import torch
+from transformers import BertTokenizer, BertModel
+import matplotlib.pyplot as plt
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+# Load pre-trained model tokenizer (vocabulary)
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+
+text = "Here is the sentence I want embeddings for."
+marked_text = "[CLS] " + text + " [SEP]"
+
+# Tokenize our sentence with the BERT tokenizer.
+tokenized_text = tokenizer.tokenize(marked_text)
+
+# Print out the tokens.
+print(tokenized_text)
+print(list(tokenizer.vocab.keys())[5000:5020])
+
+# Define a new example sentence with multiple meanings of the word "bank"
+text = "After stealing money from the bank vault, the bank robber was seen " \
+       "fishing on the Mississippi river bank."
+
+# Add the special tokens.
+marked_text = "[CLS] " + text + " [SEP]"
+
+# Split the sentence into tokens.
+tokenized_text = tokenizer.tokenize(marked_text)
+
+# Map the token strings to their vocabulary indeces.
+indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_text)
+
+# Display the words with their indeces.
+for tup in zip(tokenized_text, indexed_tokens):
+    print('{:<12} {:>6,}'.format(tup[0], tup[1]))
+
+# Mark each of the 22 tokens as belonging to sentence "1".
+segments_ids = [1] * len(tokenized_text)
+
+print(segments_ids)
+
+# Convert inputs to PyTorch tensors
+tokens_tensor = torch.tensor([indexed_tokens])
+segments_tensors = torch.tensor([segments_ids])
+
+# Load pre-trained model (weights)
+model = BertModel.from_pretrained('bert-base-uncased',
+                                  output_hidden_states=True,  # Whether the model returns all hidden-states.
+                                  )
+
+# Put the model in "evaluation" mode, meaning feed-forward operation.
+model.eval()
+
+# Run the text through BERT, and collect all of the hidden states produced
+# from all 12 layers.
+with torch.no_grad():
+    outputs = model(tokens_tensor, segments_tensors)
+
+    # Evaluating the model will return a different number of objects based on
+    # how it's  configured in the `from_pretrained` call earlier. In this case,
+    # becase we set `output_hidden_states = True`, the third item will be the
+    # hidden states from all layers. See the documentation for more details:
+    # https://huggingface.co/transformers/model_doc/bert.html#bertmodel
+    hidden_states = outputs[2]
+
+print("Number of layers:", len(hidden_states), "  (initial embeddings + 12 BERT layers)")
+
+layer_i = 0
+print("Number of batches:", len(hidden_states[layer_i]))
+
+batch_i = 0
+print("Number of tokens:", len(hidden_states[layer_i][batch_i]))
+
+token_i = 0
+print("Number of hidden units:", len(hidden_states[layer_i][batch_i][token_i]))
+
+# For the 5th token in our sentence, select its feature values from layer 5.
+token_i = 5
+layer_i = 5
+vec = hidden_states[layer_i][batch_i][token_i]
+
+# Plot the values as a histogram to show their distribution.
+plt.figure(figsize=(10, 10))
+plt.hist(vec, bins=200)
+# plt.show()
+
+# `hidden_states` is a Python list.
+print('Type of hidden_states: ', type(hidden_states))
+
+# Each layer in the list is a torch tensor.
+print('Tensor shape for each layer: ', hidden_states[0].size())
