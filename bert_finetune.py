2e7ecd677a5288d41a3c3b5a343214094870d9eb
Wenyuan Wu <wenyuan.wu@uzh.ch>
2021-08-06 04:06:39 +0200

    bert_finetune.py updated

--- bert_finetune.py
+++ bert_finetune.py
@@ -10,6 +10,8 @@ import numpy as np
 import time
 import datetime
 import random
+from sklearn.metrics import matthews_corrcoef
+import os
 
 # # Get the GPU device name.
 # device_name = tf.test.gpu_device_name()
@@ -443,3 +445,143 @@ df_stats = df_stats.set_index('epoch')
 # Display the table.
 print(df_stats)
 
+# Load the dataset into a pandas dataframe.
+df = pd.read_csv("./cola_public/raw/out_of_domain_dev.tsv", delimiter='\t', header=None,
+                 names=['sentence_source', 'label', 'label_notes', 'sentence'])
+
+# Report the number of sentences.
+print('Number of test sentences: {:,}\n'.format(df.shape[0]))
+
+# Create sentence and label lists
+sentences = df.sentence.values
+labels = df.label.values
+
+# Tokenize all of the sentences and map the tokens to thier word IDs.
+input_ids = []
+attention_masks = []
+
+# For every sentence...
+for sent in sentences:
+    # `encode_plus` will:
+    #   (1) Tokenize the sentence.
+    #   (2) Prepend the `[CLS]` token to the start.
+    #   (3) Append the `[SEP]` token to the end.
+    #   (4) Map tokens to their IDs.
+    #   (5) Pad or truncate the sentence to `max_length`
+    #   (6) Create attention masks for [PAD] tokens.
+    encoded_dict = tokenizer.encode_plus(
+        sent,  # Sentence to encode.
+        add_special_tokens=True,  # Add '[CLS]' and '[SEP]'
+        max_length=64,  # Pad & truncate all sentences.
+        pad_to_max_length=True,
+        return_attention_mask=True,  # Construct attn. masks.
+        return_tensors='pt',  # Return pytorch tensors.
+    )
+
+    # Add the encoded sentence to the list.
+    input_ids.append(encoded_dict['input_ids'])
+
+    # And its attention mask (simply differentiates padding from non-padding).
+    attention_masks.append(encoded_dict['attention_mask'])
+
+# Convert the lists into tensors.
+input_ids = torch.cat(input_ids, dim=0)
+attention_masks = torch.cat(attention_masks, dim=0)
+labels = torch.tensor(labels)
+
+# Set the batch size.
+batch_size = 32
+
+# Create the DataLoader.
+prediction_data = TensorDataset(input_ids, attention_masks, labels)
+prediction_sampler = SequentialSampler(prediction_data)
+prediction_dataloader = DataLoader(prediction_data, sampler=prediction_sampler, batch_size=batch_size)
+
+# Prediction on test set
+
+print('Predicting labels for {:,} test sentences...'.format(len(input_ids)))
+
+# Put model in evaluation mode
+model.eval()
+
+# Tracking variables
+predictions, true_labels = [], []
+
+# Predict
+for batch in prediction_dataloader:
+    # Add batch to GPU
+    batch = tuple(t.to(device) for t in batch)
+
+    # Unpack the inputs from our dataloader
+    b_input_ids, b_input_mask, b_labels = batch
+
+    # Telling the model not to compute or store gradients, saving memory and
+    # speeding up prediction
+    with torch.no_grad():
+        # Forward pass, calculate logit predictions
+        outputs = model(b_input_ids, token_type_ids=None,
+                        attention_mask=b_input_mask)
+
+    logits = outputs[0]
+
+    # Move logits and labels to CPU
+    logits = logits.detach().cpu().numpy()
+    label_ids = b_labels.to('cpu').numpy()
+
+    # Store predictions and true labels
+    predictions.append(logits)
+    true_labels.append(label_ids)
+
+print('    DONE.')
+
+print('Positive samples: %d of %d (%.2f%%)' % (df.label.sum(), len(df.label), (df.label.sum() / len(df.label) * 100.0)))
+
+matthews_set = []
+
+# Evaluate each test batch using Matthew's correlation coefficient
+print('Calculating Matthews Corr. Coef. for each batch...')
+
+# For each input batch...
+for i in range(len(true_labels)):
+    # The predictions for this batch are a 2-column ndarray (one column for "0"
+    # and one column for "1"). Pick the label with the highest value and turn this
+    # in to a list of 0s and 1s.
+    pred_labels_i = np.argmax(predictions[i], axis=1).flatten()
+
+    # Calculate and store the coef for this batch.
+    matthews = matthews_corrcoef(true_labels[i], pred_labels_i)
+    matthews_set.append(matthews)
+
+# Combine the results across all batches.
+flat_predictions = np.concatenate(predictions, axis=0)
+
+# For each sample, pick the label (0 or 1) with the higher score.
+flat_predictions = np.argmax(flat_predictions, axis=1).flatten()
+
+# Combine the correct labels for each batch into a single list.
+flat_true_labels = np.concatenate(true_labels, axis=0)
+
+# Calculate the MCC
+mcc = matthews_corrcoef(flat_true_labels, flat_predictions)
+
+print('Total MCC: %.3f' % mcc)
+
+# # Saving best-practices: if you use defaults names for the model, you can reload it using from_pretrained()
+#
+# output_dir = './model_save/'
+#
+# # Create output directory if needed
+# if not os.path.exists(output_dir):
+#     os.makedirs(output_dir)
+#
+# print("Saving model to %s" % output_dir)
+#
+# # Save a trained model, configuration and tokenizer using `save_pretrained()`.
+# # They can then be reloaded using `from_pretrained()`
+# model_to_save = model.module if hasattr(model, 'module') else model  # Take care of distributed/parallel training
+# model_to_save.save_pretrained(output_dir)
+# tokenizer.save_pretrained(output_dir)
+#
+# # Good practice: save your training arguments together with the trained model
+# # torch.save(args, os.path.join(output_dir, 'training_args.bin'))
+#
