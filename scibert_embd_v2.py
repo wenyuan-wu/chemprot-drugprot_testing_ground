edb5a6ec69a9885abb1bf8ec14d2a353b35a63dd
Wenyuan Wu <wenyuan.wu@uzh.ch>
2021-08-06 03:58:26 +0200

    bert_finetune.py updated

---
+++ scibert_embd_v2.py
@@ -0,0 +1,178 @@
+import torch
+from transformers import BertTokenizer, BertModel
+import matplotlib.pyplot as plt
+import logging
+from scipy.spatial.distance import cosine
+
+
+logging.basicConfig(level=logging.INFO)
+
+# Load pre-trained model tokenizer (vocabulary)
+tokenizer = BertTokenizer.from_pretrained('allenai/scibert_scivocab_uncased')
+
+text = "Here is the sentence I want embeddings for."
+marked_text = "[CLS] " + text + " [SEP]"
+
+# Tokenize our sentence with the BERT tokenizer.
+tokenized_text = tokenizer.tokenize(marked_text)
+
+# Print out the tokens.
+print(tokenized_text)
+print(list(tokenizer.vocab.keys())[5000:5020])
+
+# Define a new example sentence with multiple meanings of the word "bank"
+text = "After stealing money from the bank vault, the bank robber was seen " \
+       "fishing on the Mississippi river bank."
+
+# Add the special tokens.
+marked_text = "[CLS] " + text + " [SEP]"
+
+# Split the sentence into tokens.
+tokenized_text = tokenizer.tokenize(marked_text)
+
+# Map the token strings to their vocabulary indeces.
+indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_text)
+
+# Display the words with their indeces.
+for tup in zip(tokenized_text, indexed_tokens):
+    print('{:<12} {:>6,}'.format(tup[0], tup[1]))
+
+# Mark each of the 22 tokens as belonging to sentence "1".
+segments_ids = [1] * len(tokenized_text)
+
+print(segments_ids)
+
+# Convert inputs to PyTorch tensors
+tokens_tensor = torch.tensor([indexed_tokens])
+segments_tensors = torch.tensor([segments_ids])
+
+# Load pre-trained model (weights)
+model = BertModel.from_pretrained('bert-base-uncased',
+                                  output_hidden_states=True,  # Whether the model returns all hidden-states.
+                                  )
+
+# Put the model in "evaluation" mode, meaning feed-forward operation.
+model.eval()
+
+# Run the text through BERT, and collect all of the hidden states produced
+# from all 12 layers.
+with torch.no_grad():
+    outputs = model(tokens_tensor, segments_tensors)
+
+    # Evaluating the model will return a different number of objects based on
+    # how it's  configured in the `from_pretrained` call earlier. In this case,
+    # becase we set `output_hidden_states = True`, the third item will be the
+    # hidden states from all layers. See the documentation for more details:
+    # https://huggingface.co/transformers/model_doc/bert.html#bertmodel
+    hidden_states = outputs[2]
+
+print("Number of layers:", len(hidden_states), "  (initial embeddings + 12 BERT layers)")
+
+layer_i = 0
+print("Number of batches:", len(hidden_states[layer_i]))
+
+batch_i = 0
+print("Number of tokens:", len(hidden_states[layer_i][batch_i]))
+
+token_i = 0
+print("Number of hidden units:", len(hidden_states[layer_i][batch_i][token_i]))
+
+# For the 5th token in our sentence, select its feature values from layer 5.
+token_i = 5
+layer_i = 5
+vec = hidden_states[layer_i][batch_i][token_i]
+
+# Plot the values as a histogram to show their distribution.
+# TODO: runs slowly
+# plt.figure(figsize=(10, 10))
+# plt.hist(vec, bins=200)
+# plt.show()
+
+# `hidden_states` is a Python list.
+print('Type of hidden_states: ', type(hidden_states))
+
+# Each layer in the list is a torch tensor.
+print('Tensor shape for each layer: ', hidden_states[0].size())
+
+# Concatenate the tensors for all layers. We use `stack` here to
+# create a new dimension in the tensor.
+token_embeddings = torch.stack(hidden_states, dim=0)
+
+print(token_embeddings.size())
+
+# Remove dimension 1, the "batches".
+token_embeddings = torch.squeeze(token_embeddings, dim=1)
+
+print(token_embeddings.size())
+
+# Swap dimensions 0 and 1.
+token_embeddings = token_embeddings.permute(1, 0, 2)
+
+print(token_embeddings.size())
+
+# Stores the token vectors, with shape [22 x 3,072]
+token_vecs_cat = []
+
+# `token_embeddings` is a [22 x 12 x 768] tensor.
+
+# For each token in the sentence...
+for token in token_embeddings:
+    # `token` is a [12 x 768] tensor
+
+    # Concatenate the vectors (that is, append them together) from the last
+    # four layers.
+    # Each layer vector is 768 values, so `cat_vec` is length 3,072.
+    cat_vec = torch.cat((token[-1], token[-2], token[-3], token[-4]), dim=0)
+
+    # Use `cat_vec` to represent `token`.
+    token_vecs_cat.append(cat_vec)
+
+print('Shape is: %d x %d' % (len(token_vecs_cat), len(token_vecs_cat[0])))
+
+# Stores the token vectors, with shape [22 x 768]
+token_vecs_sum = []
+
+# `token_embeddings` is a [22 x 12 x 768] tensor.
+
+# For each token in the sentence...
+for token in token_embeddings:
+    # `token` is a [12 x 768] tensor
+
+    # Sum the vectors from the last four layers.
+    sum_vec = torch.sum(token[-4:], dim=0)
+
+    # Use `sum_vec` to represent `token`.
+    token_vecs_sum.append(sum_vec)
+
+print('Shape is: %d x %d' % (len(token_vecs_sum), len(token_vecs_sum[0])))
+
+# `hidden_states` has shape [13 x 1 x 22 x 768]
+
+# `token_vecs` is a tensor with shape [22 x 768]
+token_vecs = hidden_states[-2][0]
+
+# Calculate the average of all 22 token vectors.
+sentence_embedding = torch.mean(token_vecs, dim=0)
+
+print("Our final sentence embedding vector of shape:", sentence_embedding.size())
+
+for i, token_str in enumerate(tokenized_text):
+    print(i, token_str)
+
+print('First 5 vector values for each instance of "bank".')
+print('')
+print("bank vault   ", str(token_vecs_sum[6][:5]))
+print("bank robber  ", str(token_vecs_sum[10][:5]))
+print("river bank   ", str(token_vecs_sum[19][:5]))
+
+# Calculate the cosine similarity between the word bank
+# in "bank robber" vs "river bank" (different meanings).
+diff_bank = 1 - cosine(token_vecs_sum[10], token_vecs_sum[19])
+
+# Calculate the cosine similarity between the word bank
+# in "bank robber" vs "bank vault" (same meaning).
+same_bank = 1 - cosine(token_vecs_sum[10], token_vecs_sum[6])
+
+print('Vector similarity for  *similar*  meanings:  %.2f' % same_bank)
+print('Vector similarity for *different* meanings:  %.2f' % diff_bank)
+
